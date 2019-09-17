727608fc3266abb47b02d6078a119bc71ae01631
Dom <Dom@192.168.0.136>
2019-09-16 21:25:30 -0400

    able to create a file

--- demo/src/main/java/com/campuslife/demo/DemoApplication.java
+++ demo/src/main/java/com/campuslife/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.campuslife.demo;
 
+import java.io.File;
+import java.util.Formatter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,21 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		File x = new File("CampusLifeGames.txt");
+		
+		if(!x.exists()) {
+			final Formatter y;
+			
+			try {
+				y = new Formatter("CampusLifeGames.txt");
+				System.out.println(x + " created");
+			}
+			catch(Exception e) {
+				System.out.println("Something went wrong...");
+			}
+			
+		}
 	}
 
 }
