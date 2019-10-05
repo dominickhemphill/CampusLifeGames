7d5f7ccce0212f7030ba2ff6355288a062d3c1ce
Dom <Dom@192.168.0.136>
2019-10-05 17:51:23 -0400

    Corrected Classes

--- demo/src/main/java/com/campuslife/games/FileContentDisplayer.java
+++ demo/src/main/java/com/campuslife/games/FileContentDisplayer.java
@@ -7,57 +7,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileContentDisplayer {
-	
-	GameDto game;
-	GameWarehouseDto warehouse;
-	List<String> words;
-	List<GameDto> actualGames;
-	
+
 	// will display contents of file
 	public GameWarehouseDto displayContent() throws IOException {
-		
-		words = new ArrayList<>();
-		// finds file
+
+		GameDto game;
+		List<GameDto> actualGames = new ArrayList<>();
+		List<String> collection = new ArrayList<>();
+		GameWarehouseDto collections = new GameWarehouseDto();
 		FileReader file = new FileReader("CampusLifeGames.txt");
-		// reads file
 		BufferedReader reader = new BufferedReader(file);
-		
-		// empty string to store next line
 		String text = "";
-		// string to store current line being read
-		String line;
-		
-		line = reader.readLine();
-		
-		// while loop allows reader to continue through file
+		String line = reader.readLine();
+
 		while (line != null) {
-			actualGames = new ArrayList<>();
-			game = new GameDto();
 			text += line;
 			line = reader.readLine();
-//			words.add(line);
-			game.setName(line);
-			actualGames.add(game);
+			collection.add(line);
 		}
-		
-		warehouse.setGameList(actualGames);
-		
+
+		for (String gameName : collection) {
+			game = new GameDto();
+			game.setName(gameName);
+			actualGames.add(game);
+
+		}
+
+		collections.setGameList(actualGames);
+
 		// close file/buffered reader
 		file.close();
 		reader.close();
-		return warehouse;
-	}
-	
-	
-	
-	public GameWarehouseDto listConverter() {
-		actualGames = new ArrayList<>();
-		for(String gameRow : words ) {
-			game.setName(gameRow);	
-		}
-		actualGames.add(game);
-		warehouse.setGameList(actualGames);
-		return warehouse;
-		
+		return collections;
 	}
 }
--- demo/src/main/java/com/campuslife/games/GameController.java
+++ demo/src/main/java/com/campuslife/games/GameController.java
@@ -1,17 +1,12 @@
 package com.campuslife.games;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/")
@@ -33,34 +28,8 @@ public class GameController{
 	
 	@GetMapping(value="/")
 	public String displayPage(ModelMap map) throws IOException {
-		GameDto game;
-		
-		List<String> titles = new ArrayList<>();
-		List<GameDto> actualGames = new ArrayList<>();;
-		GameWarehouseDto collection = new GameWarehouseDto();
 		FileContentDisplayer list = new FileContentDisplayer();
-		FileReader file = new FileReader("CampusLifeGames.txt");
-		BufferedReader reader = new BufferedReader(file);
-		String text = "";
-		String line = reader.readLine();
-		
-		while (line != null) {
-			text += line;
-			line = reader.readLine();
-			titles.add(line);
-		}
-		
-		for(String gameName : titles) {
-			game = new GameDto();
-			game.setName(gameName);
-			actualGames.add(game);
-			
-		}
-		
-		collection.setGameList(actualGames);
-		
-
-		map.addAttribute("collection", collection);
+		map.addAttribute("collection", list.displayContent());
 		return "clf";
 	}
 	
